593dcb03b594f45637b1578641e1e06da86922dd
Pepe <piotr.krzempek@fdmgroup.com>
2024-06-23 14:51:23 +0200

    Initial commit

---
+++ src/main/java/com/example/Flight/Search/System/FlightSearchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Flight.Search.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSearchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSearchSystemApplication.class, args);
+	}
+
+}
